9c92115f073ec3f15403b54d8a3242d682b9f98d
Eden Pereira <edenjp@hotmail.com>
2020-03-21 17:40:14 -0300

    first test

--- src/test/java/com/studentapp/junit/FirstSerenityClass.java
+++ src/test/java/com/studentapp/junit/FirstSerenityClass.java
@@ -1,9 +1,12 @@
 package com.studentapp.junit;
 
 import io.restassured.RestAssured;
+import net.serenitybdd.junit.runners.SerenityRunner;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(SerenityRunner.class)
 public class FirstSerenityClass {
     @BeforeClass
     public static void init(){
